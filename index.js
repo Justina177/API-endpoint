466d98b9f16c5787b83ef4cf0bdf1bebf61fb37a
justina177 <justinaenenche@yahoo.com>
2023-09-09 07:39:38 +0100

    updated index.js

--- index.js
+++ index.js
@@ -1,22 +1,54 @@
-const express = require('express');
-const env = require("dotenv")
+import express from 'express';
+const app = express();
+const port = process.env.PORT || 7000;
 
-const port = process.env.PORT || 8000
-const app  = express ();
-app.use(express.json());
+app.get('/api', (req, res) => {
+  const { slack_name, track } = req.query;
+  
 
-app.listen(port, () => 
-    console.log(`Server Running on port ${port}!`)
-);
+  if (!slack_name || !track) {
+    
+    
+    return res.status(400).json({ error: 'slack_name and track are required parameters' });
+  }
 
-app.get("/api/justy/backend", (req, res) => {
-   res.status(200)
-   .send({
-      "Slack name": "justy",
-      "current_day": "Thursday",
-      "utc_time": "2023-09-07T14:40:05Z",
-      "github_file_url": "https://github.com/Justina177/API-endpoint/blob/main/file-1",
-      "github_repo_url": "https://github.com/Justina177/API-endpoint.git",
-      "status_code": 200
-   })   
-   });
+  // Get current day of the week
+  const currentDay = new Date().toLocaleDateString('en-US', { weekday: 'long' });
+
+  // Get current UTC time
+  const now = new Date();
+
+  // Extract the date and time components
+    const year = now.getUTCFullYear();
+    const month = String(now.getUTCMonth() + 1).padStart(2, '0');
+    const day = String(now.getUTCDate()).padStart(2, '0');
+    const hours = String(now.getUTCHours()).padStart(2, '0');
+    const minutes = String(now.getUTCMinutes()).padStart(2, '0');
+    const seconds = String(now.getUTCSeconds()).padStart(2, '0');
+
+    // Format the UTC time as "yyyy-MM-ddTHH:mm:ssZ"
+    const utcTime = `${year}-${month}-${day}T${hours}:${minutes}:${seconds}Z`;
+
+
+  // Get GitHub URL of the file being run
+  const githubFileUrl = `https://github.com/Justina177/API-endpoint/blob/main/file-1`;
+
+  // Get GitHub URL of the full source code
+  const githubRepoUrl = 'https://github.com/Justina177/API-endpoint.git';
+
+  const response = {
+    slack_name,
+    current_day: currentDay,
+    utc_time: utcTime,
+    track,
+    github_file_url: githubFileUrl,
+    github_repo_url: githubRepoUrl,
+    status_code: 200
+  };
+
+  res.json(response);
+});
+
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
+});
